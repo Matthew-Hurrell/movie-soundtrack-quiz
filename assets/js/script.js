6e47b20ef72f90c084353cc121288b9de72fed8b
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-07-24 16:15:04 +0000

    Add movie quote array with quote generator based on user score

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,9 @@ const unmuteElement = document.querySelector('.unmute-div');
 const introText = document.querySelector('.intro-text');
 const introTextElement = document.querySelector('.intro-text-div');
 const backButton = document.getElementById('back-button');
+const quoteFigure = document.getElementById('quote-figure');
+const movieQuote = document.getElementById('movie-end-quote');
+const quoteCaption = document.getElementById('quote-caption');
 
 // Answer Buttons //
 
@@ -323,6 +326,63 @@ const questions = [
 
 ];
 
+// Movie Quotes //
+
+const quotes = [
+    {
+        quote: `“Well, nobody's perfect.”`,
+        caption: 'Some Like It Hot (1959)'
+    },
+    {
+        quote: `“Houston, we have a problem.”`,
+        caption: 'Apollo 13 (1995)'
+    },
+    {
+        quote: `“Take your stinking paws off me, you damned dirty ape!”`,
+        caption: 'Planet of the Apes (1968)'
+    },
+    {
+        quote: `“Frankly, my dear, I don't give a damn”`,
+        caption: 'Gone With The Wind (1939)'
+    },
+    {
+        quote: `“Toto, I've a feeling we're not in Kansas anymore.”`,
+        caption: 'The Wizard of Oz (1939)'
+    },
+    {
+        quote: `“You're gonna need a bigger boat.”`,
+        caption: 'Jaws (1975)'
+    },
+    {
+        quote: `“Just keep swimming.”`,
+        caption: 'Finding Nemo (2003)'
+    },
+    {
+        quote: `“I'll be back.”`,
+        caption: 'The Terminator (1984)'
+    },
+    {
+        quote: `“Say “Hello” to my little friend!”`,
+        caption: 'Scarface (1983)'
+    },
+    {
+        quote: `“I'll have what she's having.”`,
+        caption: 'When Harry Met Sally (1989)'
+    },
+    {
+        quote: `“Here's looking at you, kid.”`,
+        caption: 'Casablanca (1942)'
+    },
+    {
+        quote: `“Elementary, my dear Watson.”`,
+        caption: 'The Adventures of Sherlock Holmes (1939)'
+    },
+    {
+        quote: `“Made it, Ma. Top of the world!”`,
+        caption: 'White Heat (1949)'
+    }
+]
+
 // Audio Controls //
 
 function audioControls() {
@@ -514,6 +574,19 @@ function end() {
     containerOne.classList.add('hide');
     containerTwo.classList.remove('hide');
     endScore.innerText = quizScore;
+    endQuoteGenerator();
     player.pause();
 }
 
+// End Quote Generator //
+
+function endQuoteGenerator() {
+
+    for(let i = 0; i < quotes.length; i++) {
+        if(quizScore === i) {
+            movieQuote.innerText = quotes[i].quote;
+            quoteCaption.innerText = quotes[i].caption;
+        }
+    }
+
+}
