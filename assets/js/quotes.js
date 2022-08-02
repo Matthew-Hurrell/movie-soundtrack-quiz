ba64e0098f678b9265cff5f59a73478abe58351a
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2022-08-02 11:06:56 +0000

    Add quotes array and questions array into different js files

---
+++ assets/js/quotes.js
@@ -0,0 +1,58 @@
+/* Movie Quotes */
+
+// Quotes Array
+
+const quotes = [
+    {
+        quote: `“Well, nobody's perfect.”`,
+        caption: 'Some Like It Hot (1959)'
+    },
+    {
+        quote: `“Houston, we have a problem.”`,
+        caption: 'Apollo 13 (1995)'
+    },
+    {
+        quote: `“Take your stinking paws off me, you damned dirty ape!”`,
+        caption: 'Planet of the Apes (1968)'
+    },
+    {
+        quote: `“Frankly, my dear, I don't give a damn”`,
+        caption: 'Gone With The Wind (1939)'
+    },
+    {
+        quote: `“Toto, I've a feeling we're not in Kansas anymore.”`,
+        caption: 'The Wizard of Oz (1939)'
+    },
+    {
+        quote: `“You're gonna need a bigger boat.”`,
+        caption: 'Jaws (1975)'
+    },
+    {
+        quote: `“Just keep swimming.”`,
+        caption: 'Finding Nemo (2003)'
+    },
+    {
+        quote: `“I'll be back.”`,
+        caption: 'The Terminator (1984)'
+    },
+    {
+        quote: `“Say “Hello” to my little friend!”`,
+        caption: 'Scarface (1983)'
+    },
+    {
+        quote: `“I'll have what she's having.”`,
+        caption: 'When Harry Met Sally (1989)'
+    },
+    {
+        quote: `“Here's looking at you, kid.”`,
+        caption: 'Casablanca (1942)'
+    },
+    {
+        quote: `“Elementary, my dear Watson.”`,
+        caption: 'The Adventures of Sherlock Holmes (1939)'
+    },
+    {
+        quote: `“Made it, Ma. Top of the world!”`,
+        caption: 'White Heat (1949)'
+    }
+];
